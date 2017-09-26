1489703e07252e91299ad0a1233c4a76dfe228e5
Yannig Perré <yannig.perre@gmail.com>
2017-09-26 22:09:14 +0200

    Support check mode.

--- chapitre-11/library/db_schema.py
+++ chapitre-11/library/db_schema.py
@@ -13,7 +13,8 @@ def main():
             db_url = dict(required=True, type='str'),
             spec   = dict(required=True, type='json'),
             state  = dict(default="present", choices=[ "present" ]),
-        )
+        ),
+        supports_check_mode=True
     )
     db_url = module.params.get('db_url')
     spec   = module.params.get('spec')
@@ -30,9 +31,11 @@ def main():
     # Read yaml spec and apply it to current database
     for table, columns in iteritems(yaml_spec):
         if table not in db.tables:
-            new_table = db[table]
+            if not module.check_mode:
+                new_table = db[table]
             changed = True
-            after['tables'].append(table)
+            if not module.check_mode:
+                after['tables'].append(table)
         else:
             # We store columns before modification
             table_columns = []
@@ -44,7 +47,8 @@ def main():
             if column not in db[table].columns:
                 # New columns
                 after['columns'].append("%s.%s" % (table, column))
-                db[table].create_column(column, String())
+                if not module.check_mode:
+                    db[table].create_column(column, String())
                 changed=True
 
     module.exit_json(
