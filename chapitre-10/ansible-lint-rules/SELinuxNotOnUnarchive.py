f37f11d6e61e565118e01052a6c2e5939afb61df
Yannig Perré <yannig.perre@aios.sh>
2022-12-28 14:38:04 +0100

    MAJ des exemples pour ansible-lint

--- chapitre-10/ansible-lint-rules/SELinuxNotOnUnarchive.py
+++ chapitre-10/ansible-lint-rules/SELinuxNotOnUnarchive.py
@@ -1,13 +1,13 @@
-from ansiblelint import AnsibleLintRule
+from ansiblelint.rules import AnsibleLintRule
 
 
 class SELinuxNotOnUnarchive(AnsibleLintRule):
-    id = '9001'
+    id = 'no-setype-on-unarchive'
     shortdesc = 'setype not on unarchive'
     description = 'No field ``setype`` on ``unarchive`` module'
     tags = ["unarchive"]
 
-    def matchtask(self, file, task):
+    def matchtask(self, task, file):
         module = task["action"]["__ansible_module__"]
         return module == "unarchive" and \
                'setype' not in task['action']
