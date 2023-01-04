985092394ccf037edd02e85aa4f72f3e9862b770
Yannig Perré <yannig.perre@aios.sh>
2023-01-04 08:18:17 +0100

    Correction et simplication du module callback

--- chapitre-11/callback_plugins/test.py
+++ chapitre-11/callback_plugins/test.py
@@ -5,14 +5,16 @@ __metaclass__ = type
 from ansible.plugins.callback.default import CallbackModule as CallbackModule_default
 
 DOCUMENTATION = '''
-  callback: test
-  callback_type: stdout
-  requirements:
-  short_description: Adds time to play stats
-  version_added: "2.0"
-  description:
-      - This callback is a test.
-  options:
+    name: test
+    type: stdout
+    short_description: Callback test
+    version_added: 2.12
+    description:
+        - This is a test output callback for ansible-playbook.
+    extends_documentation_fragment:
+      - default_callback
+    requirements:
+      - set as stdout in configuration
 '''
 
 class CallbackModule(CallbackModule_default):
@@ -20,8 +22,9 @@ class CallbackModule(CallbackModule_default):
     CALLBACK_TYPE = 'stdout'
     CALLBACK_NAME = 'test'
 
-    def v2_runner_on_start(self, host, task):
-        pass
+    def __init__(self):
+        # make sure the expected objects are present, calling the base's __init__
+        super(CallbackModule, self).__init__()
 
     def _dump_results(self, result, indent=None, sort_keys=True, keep_invocation=False):
         return CallbackModule_default._dump_results(self, result, indent=4, sort_keys=True, keep_invocation=False)
